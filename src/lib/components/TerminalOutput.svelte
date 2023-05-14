4a85ead79aea20587e6d067fa4d425d92afa7c4c
Radu Alexandru <alexradu95@proton.me>
2023-05-14 14:10:15 +0300

    WIP

---
+++ src/lib/components/TerminalOutput.svelte
@@ -0,0 +1,21 @@
+<!-- TerminalOutput.svelte -->
+<script>
+	import UserPrompt from "./UserPrompt.svelte"
+    export let lineData = []
+</script>
+
+<pre class="output">Type 'help' to learn more.</pre>
+{#each lineData as line, i (i)}
+    <span>
+        <UserPrompt></UserPrompt>
+        <pre class="input-old">{line.command}</pre>
+        <br />
+        {#if typeof line.output === 'string'}
+            <pre class="output">{line.output}</pre>
+        {:else}
+            {#each line.output as out}
+                {@html out}
+            {/each}
+        {/if}
+    </span>
+{/each}
---
+++ src/lib/components/UserPrompt.svelte
@@ -0,0 +1,6 @@
+<!-- Prompt.svelte -->
+<script>
+    import { user, machine } from '$lib/stores'
+</script>
+
+<p class="prompt">{$user}@{$machine}:$&nbsp;</p>
