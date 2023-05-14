6d4971ed6df005d236aeadfbf02ceee7eb8e5016
Radu Alexandru <alexradu95@proton.me>
2023-05-14 13:29:40 +0300

    Init

---
+++ src/lib/bookmarks.js
@@ -0,0 +1,34 @@
+export const bookmarks = {
+	general: {
+		inbox: 'https://mail.google.com/',
+		cal: 'https://calendar.google.com/',
+		drive: 'https://drive.google.com/',
+		amazon: 'https://amazon.com/',
+		'amazon-jp': 'https://amazon.co.jp/',
+	},
+
+	media: {
+		yt: 'https://youtube.com/feed/subscriptions',
+		netflix: 'https://netflix.com/',
+		plex: 'https://app.plex.tv',
+	},
+
+	tech: {
+		github: 'http://github.com/',
+		speedtest: 'http://speedtest.net/',
+		mdn: 'https://developer.mozilla.org/en-US/',
+		src: 'http://github.com/josephkhawly/svelte-terminal',
+		vercel: 'https://vercel.com/',
+	},
+
+	work: {
+		notion: 'https://www.notion.so/',
+		jira: 'https://mwmdigital.atlassian.net/jira/projects?selectedProjectType=software',
+		bitbucket: 'https://bitbucket.org/',
+	},
+
+	social: {
+		twitter: 'https://twitter.com/',
+		linkedin: 'https://linkedin.com/in/josephkhawly',
+	},
+}
